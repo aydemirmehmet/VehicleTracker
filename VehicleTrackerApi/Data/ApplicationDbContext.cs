875af3725c74205c731fc2f56375d6b01666164c
mehmet <coderaydemir@gmail.com>
2021-10-01 11:47:18 +0300

    initial Entity Framework

---
+++ VehicleTrackerApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace VehicleApi.Contexts
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+         
+         
+
+
+        }
+
+    }
+}
