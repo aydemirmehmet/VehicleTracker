fdd1acc517ae27e87a3344380ebc82ee9708fc3b
mehmet <coderaydemir@gmail.com>
2021-10-03 16:34:24 +0300

    implementation Of Dependency Injections on startup

--- VehicleTrackerApi/Startup.cs
+++ VehicleTrackerApi/Startup.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,6 +14,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using VehicleApi.Contexts;
+using VehicleTrackerApi.Mapping;
+using VehicleTrackerApi.Services;
+using VehicleTrackerApi.Services.Base;
 
 namespace VehicleTrackerApi
 {
@@ -28,10 +32,31 @@ namespace VehicleTrackerApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            #region Inject Repositories
+
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddTransient<IVehicleRepository, VehicleRepository>();
+            services.AddTransient<IVehiclePositionRepository, VehiclePositionRepository>();
+            services.AddTransient<IPlaceRepository, PlaceRepository>();
+            services.AddTransient<IReportRepository, ReportRepository>();
+            #endregion
+
+            #region Inject AutoMapper
+            var mappingConfig = new MapperConfiguration(mc => {
+                mc.AddProfile(new PlaceProfile());
+                mc.AddProfile(new ReportProfile());
+                mc.AddProfile(new VehicleProfile());
+            });
+            IMapper mapper = mappingConfig.CreateMapper();
+            services.AddSingleton(mapper);
+
+            #endregion
             services.AddDbContext<ApplicationDbContext>(options=>{
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),opt=> {
-                    opt.UseNetTopologySuite();
-                });
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"),
+                    opt=> {
+                          opt.UseNetTopologySuite();
+                      });
             });
             services.AddControllers();
 
